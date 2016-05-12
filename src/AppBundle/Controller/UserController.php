eaf04e8294f71c238d45da32e82de19a62f1ace2
Deimokle <q.dutrevis@gmail.com>
2016-05-12 21:05:34 +0200

    prifile prenom

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -13,9 +13,11 @@ class UserController extends Controller
      */
     public function userAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $request = $em->getRepository('AppBundle:User')->findOneById($user->getId());
         return $this->render('default/profil.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            'requete' => $request,
         ));
     }
 }
