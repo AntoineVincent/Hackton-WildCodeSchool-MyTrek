ba065b7c786bf4dc45033a5f65154df91fc5fff9
Antoine <anton51200@laposte.net>
2016-05-12 16:42:05 +0200

    create entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -23,5 +24,208 @@ class User extends BaseUser
     {
         parent::__construct();
         // your own logic
+    }
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", nullable=true)
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", nullable=true)
+     */
+    private $firstname;
+    /**
+     * @var date
+     *
+     * @ORM\Column(name="age", type="date", nullable=true)
+     */
+    private $age;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="gender", type="string", nullable=true)
+     */
+    private $gender;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="level", type="integer", nullable=true)
+     */
+    private $level;
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="exp", type="integer", nullable=true)
+     */
+    private $exp;
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="treks", type="array", nullable=true)
+     */
+    private $treks;
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     * @return User
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string 
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set age
+     *
+     * @param \DateTime $age
+     * @return User
+     */
+    public function setAge($age)
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    /**
+     * Get age
+     *
+     * @return \DateTime 
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    /**
+     * Set gender
+     *
+     * @param string $gender
+     * @return User
+     */
+    public function setGender($gender)
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * Get gender
+     *
+     * @return string 
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     * @return User
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return integer 
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set exp
+     *
+     * @param integer $exp
+     * @return User
+     */
+    public function setExp($exp)
+    {
+        $this->exp = $exp;
+
+        return $this;
+    }
+
+    /**
+     * Get exp
+     *
+     * @return integer 
+     */
+    public function getExp()
+    {
+        return $this->exp;
+    }
+
+    /**
+     * Set treks
+     *
+     * @param array $treks
+     * @return User
+     */
+    public function setTreks($treks)
+    {
+        $this->treks = $treks;
+
+        return $this;
+    }
+
+    /**
+     * Get treks
+     *
+     * @return array 
+     */
+    public function getTreks()
+    {
+        return $this->treks;
     }
 }
