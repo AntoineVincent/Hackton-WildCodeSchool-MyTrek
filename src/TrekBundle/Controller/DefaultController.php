63fccd226da1e94c02dbd0c5cda4b860d4156b55
etidimjav <thomas.javanaud@gmail.com>
2016-05-12 16:44:33 +0200

    Init du trekbundle

---
+++ src/TrekBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TrekBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('TrekBundle:Default:index.html.twig');
+    }
+}
